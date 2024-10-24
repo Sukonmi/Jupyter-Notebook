e11a134288787eb706088629c73e41a4bb2faf43
Sukonmi <danielayomideh@gmail.com>
2024-10-24 11:32:01 +0100

    I edited the Bank Analysis code

--- grades_analysis.py
+++ grades_analysis.py
@@ -25,3 +25,34 @@ print(f"The maximum grade is {max_grade}")
 # Minimum value
 min_grade = np.min(grades)
 print(f"The minimum grade is {min_grade}")
+
+# 4. Use numpy function to sort the grades in ascending order.
+sorted_grades = np.sort(grades)
+print(f"The sorted grades are: {sorted_grades}")
+
+# 5. Use numpy function to find the index of the highest grade in the array.
+hg_index = np.where(grades == max_grade)
+print(f"The index for the highest grade is {hg_index}")
+
+# 6. Use numpy function to count the number of students who scored above 90.
+grade_count90 = np.count_nonzero(grades > 90)
+print(f"The number of students who scored above 90: {grade_count90} students.")
+
+# 7. Use numpy function to calculate the percentage of students who scored above 90.
+total = np.sum(grades)
+grade90 = np.sum(grades[np.where(grades > 90)])
+percentage1 = round((grade90 / total) * 100)
+print(f"The percentage of students that scored above 90 is {percentage1}%")
+
+# 8. Use numpy function to calculate the percentage of students who scored below 75.
+grade75 = np.sum(grades[np.where(grades < 75)])
+percentage2 = (grade75 / total) * 100
+print(f"The percentage of studets that scored below 75 is {percentage2}%")
+
+# 9. Use numpy function to extract all the grades above 90 and put them in a new array called "high_performers".
+high_performers = grades[grades > 90]
+print(high_performers)
+
+# 10. Create a new array called "passing_grades" that contains all the grades above 75.
+passing_grades = grades[grades > 75]
+print(passing_grades)
